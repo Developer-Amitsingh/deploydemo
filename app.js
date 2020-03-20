de2e3da3ca46f6ac005d8108df4fad9e099d0fa3
amitsingh <amitnietnoida@gmail.com>
2020-03-20 08:17:51 +0000

    add all file

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+
+
+
+app.get("/",function(req,res){
+	res.render("home.ejs");
+});
+
+app.get("/about",function(req,res){
+	res.render("about.ejs");
+})
+
+app.listen(process.env.PORT,process.env.IP);
